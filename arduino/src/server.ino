423c7a8c0df815bb4736b47d1bc912132ca6aea6
Canta <canta@canta.com.ar>
2016-11-12 19:18:50 -0300

    Más funciones helpers y estados.

--- arduino/src/server.ino
+++ arduino/src/server.ino
@@ -19,9 +19,11 @@ const char CYCLE_STATE_GOING_DOWN     = 2;
 const char CYCLE_STATE_PAGE_FIX       = 3;
 const char CYCLE_STATE_SCANNING       = 4;
 const char CYCLE_STATE_PAGE_TURN      = 5;
+const char CYCLE_STATE_GOING_HOME     = 6;
 
 // Global vars
 char cycle_current_direction;
+char cycle_current_status;
 
 void setup( )
 {
@@ -34,7 +36,7 @@ void setup( )
     cycle_current_direction  =  CYCLE_STATE_GOING_UP;
 }
 
-char check_plate_state()
+char plate_check_state()
 {
     char bottom = digitalRead( BIT_BOTTOM );
     char top    = digitalRead( BIT_TOP );
@@ -43,23 +45,43 @@ char check_plate_state()
     if (bottom && !top)
     {
         ret = PLATE_STATE_DOWN;
+        cycle_current_direction = CYCLE_STATE_GOING_UP;
+        plate_off();
     }
     else if (!bottom && top)
     {
         ret = PLATE_STATE_UP;
+        cycle_current_direction = CYCLE_STATE_GOING_DOWN;
+        plate_off();
     }
     else if (!bottom && !top)
     {
         ret = PLATE_STATE_MIDDLE;
+    }
+    else
+    {
+        plate_off();
     }
     
     return ret;
 }
 
 
+
 char plate_is_moving( )
 {
     return digitalRead( BIT_MOVE );
+}
+
+void plate_off()
+{
+    digitalWrite( BIT_MOVE, LOW  );
+    delay( 500 );
+}
+
+void plate_on()
+{
+    digitalWrite( BIT_MOVE, HIGH );
 }
 
 char plate_toggle( )
@@ -73,6 +95,7 @@ char plate_toggle_direction( )
 {
     char l_dir = cycle_current_direction == CYCLE_STATE_GOING_DOWN ? HIGH : LOW;
     digitalWrite( BIT_DIRECTION, l_dir );
+    cycle_current_direction = l_dir == HIGH ? CYCLE_STATE_GOING_UP : CYCLE_STATE_GOING_DOWN;
     return l_dir;
 }
 
